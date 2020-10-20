b9b0ede80e9b7706793692dd3656cc01c9dc936e
kwdaaa <kwd.aaa.a@icloud.com>
2020-10-20 10:51:18 +0900

    １回目の課題提出

--- index3.php
+++ index3.php
@@ -1,21 +1,20 @@
 <?php
 
 // 以下にCarクラスを定義して、当プログラムを実行した時にエラーがでないようにして下さい。
-Class Car{
-
+Class Car
+{
     // 変数を宣言。値は入れない。
     public $getName;
     public $getNumber;
     public $getColor;
 
-
+    // 値の初期値を設定的な...？
     public function __construct($getName, $getNumber, $getColor)
     {
         $this->getName = $getName;
         $this->getNumber = $getNumber;
         $this->getColor = $getColor;
     }
-
 
     // クラス内にgetName関数を作った。
     // 「$this->」にすることでクラスないで定義した「$getName;」を拾うことができる。
@@ -38,33 +37,48 @@ Class Car{
         echo $this->getColor;
     }
 
+    // クラス内にgetName関数を作った。
+    // 「$this->」にすることでクラスないで定義した「$getName;」を拾うことができる。
+    public function getInfomation()
+    {
+        echo '車の車種：' . $this->getName . '、車体番号：' . $this->getNumber . '、カラー：' . $this->getColor . 'です。<br>';
+    }
+}
+
+
+Class Car2
+{
+    // 変数を宣言。値は入れない。
+    public $setName;
+    public $setNumber;
+    public $setColor;
+
+
 
     // クラス内にgetName関数を作った。
     // 「$this->」にすることでクラスないで定義した「$getName;」を拾うことができる。
     public function setName($setName)
     {
-        echo $this->getName;
+        echo $this->setName = $setName;
     }
 
     // クラス内にgetName関数を作った。
     // 「$this->」にすることでクラスないで定義した「$getName;」を拾うことができる。
     public function setNumber($setNumber)
     {
-        echo $this->getNumber;
+        echo $setNumber;
     }
 
     // クラス内にgetName関数を作った。
     // 「$this->」にすることでクラスないで定義した「$getName;」を拾うことができる。
     public function setColor($setColor)
     {
-        echo $this->getColor;
+        echo $setColor;
     }
 
-    // クラス内にgetName関数を作った。
-    // 「$this->」にすることでクラスないで定義した「$getName;」を拾うことができる。
-    public function infomation($infomation)
+    public function setInfomation()
     {
-        echo '車の車種：' . $this->getName . '、車体番号：' . $this->getNumber . '、カラー' . $this->age . 'です。<br>';
+        echo '車の車種：' . $this->setName . '、車体番号：' . $this->setNumber . '、カラー：' . $this->setColor . 'です。<br>';
     }
 }
 
@@ -78,35 +92,37 @@ $car1 = new Car('ランクル', 100, 'black');
 
 // 車名(車種)を取得
 echo $car1->getName();
-echo $getName . '<br>';
+echo '<br>';
 
 // 車体番号を取得
 echo $car1->getNumber();
-echo $getNumber . '<br>';
+echo '<br>';
 
 // カラーを取得
 echo $car1->getColor();
-echo $getColor . '<br>';
+echo '<br>';
 
 // 車の情報表示を表示
-// echo $car1->infomation();
-// echo '<br>';
+echo $car1->getInfomation();
+echo '<br>';
+
+$car2 = new Car2();
 
 // 車名(車種)を更新
-$car1->setName('アルファード');
-echo $car1->getName();
-echo $setName . '<br>';
+$car2->setName('アルファード');
+echo $car2->setName();
+echo '<br>';
 
 // 車体番号を取得
-$car1->setNumber(200);
-echo $car1->getNumber();
-echo $setNumber . '<br>';
+$car2->setNumber(200);
+echo $car2->setNumber();
+echo '<br>';
 
 // カラーを取得
-$car1->setColor('red');
-echo $car1->getColor();
-echo $setColor . '<br>';
+$car2->setColor('red');
+echo $car2->setColor();
+echo '<br>';
 
 // 車の情報表示を表示
-// echo $car1->infomation();
-// echo '<br>';
+echo $car2->setInfomation();
+echo '<br>';
